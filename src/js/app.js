fec096890f6d89405c403be379ee4c359e5f0bca
unknown <kgeluza@mail.ru>
2022-01-31 21:04:07 +0400

    commit 31/01

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 // TODO: write your code here
 
-const cellArr = Array.from(document.getElementsByClassName('cell'));
-const activeCell = document.getElementsByClassName('cell_with_char')[0];
-const arrLength = cellArr.length;
-const cell = (index) => document.getElementById(`cell${index}`);
+function randomGame() {
+    setInterval
+    const cellArr = Array.from(document.getElementsByClassName('cell'));
+    const arrLength = cellArr.length;
+    const activeIndex = cellArr.findIndex((cell) => cell.classList.contains('cell_with_char'));
+    cellArr[activeIndex].classList.remove('cell_with_char');
+    const activation = Math.floor(1 + Math.random() * arrLength);
+    cellArr[activation].classList.add('cell_with_char');
+}
 
-const activation = Math.floor(1 + Math.random() * arrLength);
-const randomGame = setInterval(() => activeCell.classList.remove('cell_with_char'),
-  cellArr[activation].classList.add('cell_with_char'), 1000);
-
-randomGame();
+setInterval(randomGame, 1000);
