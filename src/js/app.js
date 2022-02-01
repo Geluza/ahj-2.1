d4ae1a648e3c3927be239d10d5f6b0f90761c63a
unknown <kgeluza@mail.ru>
2022-02-01 22:33:33 +0400

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,14 @@
 // TODO: write your code here
 
 function randomGame() {
-    setInterval
     const cellArr = Array.from(document.getElementsByClassName('cell'));
     const arrLength = cellArr.length;
     const activeIndex = cellArr.findIndex((cell) => cell.classList.contains('cell_with_char'));
     cellArr[activeIndex].classList.remove('cell_with_char');
-    const activation = Math.floor(1 + Math.random() * arrLength);
+    let activation = Math.floor(Math.random() * arrLength);
+    if(activeIndex === activation) {
+        activation = +1;
+    }
     cellArr[activation].classList.add('cell_with_char');
 }
 
